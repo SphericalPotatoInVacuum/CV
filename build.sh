44fd6c14c2e4ce0269b81371da858437f8524bcd
Alexander Bogdanov <a@bogdanov.co>
2022-10-05 05:57:15 +0500

    👷 Add output directory option

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-dir=$(dirname $1)
-base=$(basename $1)
-cd "${dir}"
-if ! xelatex -interaction=nonstopmode "${base}"; then
+root=$(pwd)
+
+src_dir=$(dirname $1)
+src_base=$(basename $1)
+
+out_dir=$2
+out_pdf="${src_base%.*}.pdf"
+
+cd "${src_dir}"
+if ! xelatex -interaction=nonstopmode "${src_base}"; then
     echo "Compilation failed"
     exit 1
 fi
+
 rm *.aux *.out *.log
+
+cd "${root}"
+mkdir -p "${out_dir}"
+mv "${src_dir}/${out_pdf}" "${out_dir}/${out_pdf}"
